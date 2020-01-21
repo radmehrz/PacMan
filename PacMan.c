f9b27d694c013b038ac7bbb6fa28d47161877a8c
radmehrz <58809469+radmehrz@users.noreply.github.com>
2020-01-21 17:56:30 +0330

    Merge ai play with selfplay

--- PacMan.c
+++ PacMan.c
@@ -14,6 +14,7 @@ char cFruit = '*';
 bool wall = true;
 
 
+void gotoxy();
 typedef struct point{
 	int cx;
 	int cy;
@@ -32,7 +33,7 @@ enum option{aiplay,selfplay,settings,quit};
 point map[5][10];
 int X;
 int Y;
-Star star; 
+Star star;
 
 void init_map(int maxX, int maxY){
 	char temp;
@@ -103,7 +104,50 @@ void printMap(int x , int y){
 	endl
 }
 
+void findWay(int startX, int startY , int maxX , int maxY){
+	if(map[startX+1][startY].data!=cBlock &&!map[startX+1][startY].visited && startX != maxX-1)
+	{
+		map[startX+1][startY].visited = true;
+		map[startX+1][startY].cx = -1;
+		map[startX+1][startY].cy = 0;
+		findWay(startX+1,startY,maxX,maxY);
+	}
+	if(map[startX-1][startY].data != cBlock &&!map[startX-1][startY].visited && startX != 0)
+	{
+		map[startX-1][startY].visited = true;
+		map[startX-1][startY].cx = 1;
+		map[startX-1][startY].cy = 0;
+		findWay(startX-1,startY,maxX,maxY);
+	}
+	if(map[startX][startY-1].data != cBlock &&!map[startX][startY-1].visited && startY != 0)
+	{
+		map[startX][startY-1].visited = true;
+		map[startX][startY-1].cx = 0;
+		map[startX][startY-1].cy = +1;
+		findWay(startX,startY-1,maxX,maxY);
+	}
+	if(map[startX][startY+1].data != cBlock &&!map[startX][startY+1].visited && startY != maxY-1)
+	{
+		map[startX][startY+1].visited = true;
+		map[startX][startY+1].cx = 0;
+		map[startX][startY+1].cy = -1;
+		findWay(startX,startY+1,maxX,maxY);
+	}
+}
 
+
+
+void move(){
+	gotoxy(Y+1,X+1);
+	printf("%c" , map[X][Y].data);
+	int newX = map[X][Y].cx;
+	int newY = map[X][Y].cy;
+	X += newX;
+	Y += newY;
+	gotoxy(Y+1,X+1);
+	printf("%c\b" , cPlayer);
+	
+}
 
 void findStar(int maxX,int maxY){
 	for(int i = 0;i < maxX;i++)
@@ -121,6 +165,15 @@ void findStar(int maxX,int maxY){
 	star.y = -1;
 }
 
+void clearWay(int maxX,int maxY){
+	for(int i = 0;i < maxX;i++)
+	for(int j = 0;j < maxY;j++)
+	{
+		map[i][j].cx = -1;
+		map[i][j].cy = -1;
+		map[i][j].visited = false;
+	}
+}
 
 bool checkEnd(int maxX,int maxY){
 	if(map[X][Y].data==cFruit)
@@ -128,6 +181,8 @@ bool checkEnd(int maxX,int maxY){
 			map[X][Y].data= cEmpty;
 			findStar(maxX,maxY);
 			if(star.x == -1) return true;
+			clearWay(maxX,maxY);
+			findWay(star.x,star.y,maxX,maxY);
 
 		}
 		return false;
@@ -157,7 +212,122 @@ int getKey(){
 	}
 }
 
+int menu(){
+	int choose = 0;
+	while(1)
+	{
+		system("@cls||clear");
+		printf("Menu: \n");
+		printf("_________________________\n");
+		printf("| %c Auto Play           |\n" , choose == 0?'>':' ');
+		printf("|_______________________|\n");
+		printf("| %c Self Play           |\n" , choose == 1?'>':' ');
+		printf("|_______________________|\n");
+		printf("| %c Settings            |\n" , choose == 2?'>':' ');
+		printf("|_______________________|\n");
+		printf("| %c Exit                |\n" , choose == 3?'>':' ');
+		printf("|_______________________|\n");
+		while(1)
+		{
+			char ch = getKey();
+			if(ch == up && choose != 0)
+			{
+				choose--;
+				break;
+			}
+			if(ch == down && choose != 3)
+			{
+				choose++;
+				break;
+			}
+			if(ch == enter)
+			{
+				return choose;
+			}
+			if(ch == esc)
+			{
+				return quit;
+			}
+			
+		}
+	}
+}
 
+void options(){
+	int choose = 0;
+	while(1)
+	{
+		system("@cls||clear");
+		printf("Settings: \n");
+		printf("__________________________\n");
+		printf("| %c PacMan Character : %c |\n", choose == 0?'>':' ' , cPlayer);
+		printf("|________________________|\n");
+		printf("| %c Fruit Character :  %c |\n" , choose == 1?'>':' ' , cFruit);
+		printf("|________________________|\n");
+		printf("| %c Solid Wall         %c |\n" , choose == 2?'>':' ' , wall?'Y':'N');
+		printf("|________________________|\n");
+		printf("| %c Quit                 |\n" , choose == 3?'>':' ');
+		printf("|________________________|\n");
+		while(1)
+		{
+			char ch = getKey();
+			if(ch == up && choose != 0)
+			{
+				choose--;
+				break;
+			}
+			if(ch == down && choose != 3)
+			{
+				choose++;
+				break;
+			}
+			if(ch == enter)
+			{
+				if(choose == 0)
+				{
+					fflush(stdin);
+					system("@cls||clear");
+					printf("Enter new character: ");
+					scanf("%c" , &cPlayer);
+					if(cPlayer == cEmpty || cPlayer == cBlock || cPlayer == cFruit)
+					{
+						printf("Error! %c has taken.", cPlayer);
+						cFruit = '*';
+						cPlayer = 'O';
+					}
+					fflush(stdin);
+					break;
+				}
+				if(choose == 1)
+				{
+					fflush(stdin);
+					system("@cls||clear");
+					printf("Enter new character: ");
+					scanf("%c" , &cFruit);
+					if(cFruit == cEmpty || cFruit == cBlock || cPlayer == cFruit)
+					{
+						printf("Error! %c has taken.", cFruit);
+						cFruit = '*';
+						cPlayer = 'O';
+					}
+					fflush(stdin);
+					break;
+				}
+				if (choose == 2)
+				{
+					wall = !wall;
+					break;
+				}
+				if(choose == 3)return;
+			}
+			if(ch == esc)
+			{
+				return;
+			}
+			
+		}
+	}
+}
 
 void gotoxy(int eex, int eey)
 {
@@ -171,9 +341,26 @@ void gotoxy(int eex, int eey)
 int main(){
 	while(1)
 	{
-			system("@cls||clear");
+		int choose = menu();
+		if(choose == aiplay)
+		{
+			init_map(4,4);
+			system("cls");
+			printMap(4,4);
+			sleep(1);
+			findStar(4,4);
+			findWay(star.x,star.y,4,4);
+			while(1)
+			{
+				if(checkEnd(4,4)) break;
+				move();
+				sleep(1);
+			}
+		}
+		if(choose == selfplay)
+		{
 			init_map(5,10);
-			system("@cls||clear");
+			system("cls");
 			printMap(5,10);
 			while(1)
 			{
@@ -182,7 +369,13 @@ int main(){
 				if(key == up)
 				{
 					if(X == 0 && !wall && map[4][Y].data != cBlock)
+					{
+						gotoxy(Y+1,X+1);
+						printf("%c" , map[X][Y].data);
 						X = 4;
+						gotoxy(Y+1,X+1);
+						printf("%c\b" , cPlayer);
+					}
 					else if(X != 0 && map[X-1][Y].data != cBlock) 
 					{
 						gotoxy(Y+1,X+1);
@@ -195,7 +388,13 @@ int main(){
 				if(key == down)
 				{
 					if(X == 4 && !wall && map[0][Y].data != cBlock)
+					{
+						gotoxy(Y+1,X+1);
+						printf("%c" , map[X][Y].data);
 						X = 0;
+						gotoxy(Y+1,X+1);
+						printf("%c\b" , cPlayer);
+					}
 					else if(X != 4 && map[X+1][Y].data != cBlock) 
 					{
 						gotoxy(Y+1,X+1);
@@ -208,7 +407,13 @@ int main(){
 				if(key == right)
 				{
 					if(Y == 9 && !wall  && map[X][0].data != cBlock)
+					{
+						gotoxy(Y+1,X+1);
+						printf("%c" , map[X][Y].data);
 						Y = 0;
+						gotoxy(Y+1,X+1);
+						printf("%c\b" , cPlayer);
+					}
 					else if(Y != 9 && map[X][Y+1].data != cBlock) 
 					{
 						gotoxy(Y+1,X+1);
@@ -221,7 +426,13 @@ int main(){
 				if(key == left)
 				{
 					if(Y == 0 && !wall && map[X][9].data != cBlock)
+					{
+						gotoxy(Y+1,X+1);
+						printf("%c" , map[X][Y].data);
 						Y = 9;
+						gotoxy(Y+1,X+1);
+						printf("%c\b" , cPlayer);
+					}
 					else if(Y != 0 && map[X][Y-1].data != cBlock)
 					{
 						gotoxy(Y+1,X+1);
@@ -238,4 +449,14 @@ int main(){
 				
 			}
 		}
+		if(choose == settings)
+		{
+			options();
+		}
+		if(choose == quit)
+		{
+			exit(0);
+		}
 	}
+	
+}
